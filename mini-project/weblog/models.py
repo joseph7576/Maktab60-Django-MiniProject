6960b3b7648f865fec75f1043e51c901ecf7441f
Joseph <joeho7576@gmail.com>
2021-12-02 20:11:11 +0330

    Add Unique Slug Generator

--- mini-project/weblog/models.py
+++ mini-project/weblog/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.fields import SlugField
+from django.utils.text import slugify
+import random, string
+from django.db.models.signals import pre_save
+
+def random_string_generator(size = 10, chars = string.ascii_lowercase + string.digits): 
+    return ''.join(random.choice(chars) for _ in range(size)) 
+  
+def unique_slug_generator(instance, new_slug = None): 
+    if new_slug is not None: 
+        slug = new_slug 
+    else: 
+        slug = slugify(instance.title) 
+    Klass = instance.__class__ 
+    qs_exists = Klass.objects.filter(slug = slug).exists() 
+    if qs_exists: 
+        new_slug = "{slug}-{randstr}".format( 
+            slug = slug, randstr = random_string_generator(size = 4)) 
+              
+        return unique_slug_generator(instance, new_slug = new_slug) 
+    return slug 
+
+
 
 class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Post Owner")
@@ -8,11 +29,16 @@ class Post(models.Model):
     content = models.TextField()
     category = models.ManyToManyField('Category')
     created = models.DateField(auto_now_add=True, null=True)
-    slug = models.SlugField(max_length=40, unique=True, null=True)
+    slug = models.SlugField(max_length=40, unique=True, null=True, blank=True)
 
     def __str__(self):
         return self.title
 
+def pre_save_receiver(sender, instance, *args, **kwargs): 
+   if not instance.slug: 
+       instance.slug = unique_slug_generator(instance) 
+
+pre_save.connect(pre_save_receiver, sender = Post) 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
